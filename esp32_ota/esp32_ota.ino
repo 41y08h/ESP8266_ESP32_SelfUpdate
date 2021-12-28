ccfc52bc5321fb1945cc9d5daa9935eaf278401c
Piyush <piyush41y08h@gmail.com>
2021-12-28 14:50:06 +0530

    fix code and compile firmware

--- esp32_ota/esp32_ota.ino
+++ esp32_ota/esp32_ota.ino
@@ -4,18 +4,13 @@
 #include <WiFiClientSecure.h>
 #include "cert.h"
 
-const char * ssid = "home_wifi";
-const char * password = "helloworld";
+const char *ssid = "::::||::::::||:::::::||:::::";
+const char *password = "windows9";
 
+String FirmwareVer = "2.2";
 
-String FirmwareVer = {
-  "2.2"
-};
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
-
-//#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
-//#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
+#define URL_fw_Version "https://raw.githubusercontent.com/41y08h/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
+#define URL_fw_Bin "https://raw.githubusercontent.com/41y08h/ESP8266_ESP32_SelfUpdate/master/esp32_ota/firmware.bin"
 
 void connect_wifi();
 void firmwareUpdate();
@@ -25,57 +20,62 @@ unsigned long previousMillis = 0; // will store last time LED was updated
 unsigned long previousMillis_2 = 0;
 const long interval = 60000;
 const long mini_interval = 1000;
-void repeatedCall() {
-  static int num=0;
+void repeatedCall()
+{
+  static int num = 0;
   unsigned long currentMillis = millis();
-  if ((currentMillis - previousMillis) >= interval) {
+  if ((currentMillis - previousMillis) >= interval)
+  {
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-    if (FirmwareVersionCheck()) {
+    if (FirmwareVersionCheck())
+    {
       firmwareUpdate();
     }
   }
-  if ((currentMillis - previousMillis_2) >= mini_interval) {
+  if ((currentMillis - previousMillis_2) >= mini_interval)
+  {
     previousMillis_2 = currentMillis;
     Serial.print("idle loop...");
     Serial.print(num++);
     Serial.print(" Active fw version:");
     Serial.println(FirmwareVer);
-   if(WiFi.status() == WL_CONNECTED) 
-   {
-       Serial.println("wifi connected");
-   }
-   else
-   {
-    connect_wifi();
-   }
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      Serial.println("wifi connected");
+    }
+    else
+    {
+      connect_wifi();
+    }
   }
 }
 
-struct Button {
+struct Button
+{
   const uint8_t PIN;
   uint32_t numberKeyPresses;
   bool pressed;
 };
 
 Button button_boot = {
-  0,
-  0,
-  false
-};
+    0,
+    0,
+    false};
 /*void IRAM_ATTR isr(void* arg) {
     Button* s = static_cast<Button*>(arg);
     s->numberKeyPresses += 1;
     s->pressed = true;
 }*/
 
-void IRAM_ATTR isr() {
+void IRAM_ATTR isr()
+{
   button_boot.numberKeyPresses += 1;
   button_boot.pressed = true;
 }
 
-
-void setup() {
+void setup()
+{
   pinMode(button_boot.PIN, INPUT);
   attachInterrupt(button_boot.PIN, isr, RISING);
   Serial.begin(115200);
@@ -84,8 +84,10 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   connect_wifi();
 }
-void loop() {
-  if (button_boot.pressed) { //to connect wifi via Android esp touch app 
+void loop()
+{
+  if (button_boot.pressed)
+  { //to connect wifi via Android esp touch app
     Serial.println("Firmware update Starting..");
     firmwareUpdate();
     button_boot.pressed = false;
@@ -93,10 +95,12 @@ void loop() {
   repeatedCall();
 }
 
-void connect_wifi() {
+void connect_wifi()
+{
   Serial.println("Waiting for WiFi");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -107,14 +111,15 @@ void connect_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-
-void firmwareUpdate(void) {
+void firmwareUpdate(void)
+{
   WiFiClientSecure client;
   client.setCACert(rootCACertificate);
   httpUpdate.setLedPin(LED_BUILTIN, LOW);
   t_httpUpdate_return ret = httpUpdate.update(client, URL_fw_Bin);
 
-  switch (ret) {
+  switch (ret)
+  {
   case HTTP_UPDATE_FAILED:
     Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
     break;
@@ -128,7 +133,8 @@ void firmwareUpdate(void) {
     break;
   }
 }
-int FirmwareVersionCheck(void) {
+int FirmwareVersionCheck(void)
+{
   String payload;
   int httpCode;
   String fwurl = "";
@@ -136,17 +142,17 @@ int FirmwareVersionCheck(void) {
   fwurl += "?";
   fwurl += String(rand());
   Serial.println(fwurl);
-  WiFiClientSecure * client = new WiFiClientSecure;
+  WiFiClientSecure *client = new WiFiClientSecure;
 
-  if (client) 
+  if (client)
   {
-    client -> setCACert(rootCACertificate);
+    client->setCACert(rootCACertificate);
 
-    // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is 
+    // Add a scoping block for HTTPClient https to make sure it is destroyed before WiFiClientSecure *client is
     HTTPClient https;
 
-    if (https.begin( * client, fwurl)) 
-    { // HTTPS      
+    if (https.begin(*client, fwurl))
+    { // HTTPS
       Serial.print("[HTTPS] GET...\n");
       // start connection and send HTTP header
       delay(100);
@@ -155,7 +161,9 @@ int FirmwareVersionCheck(void) {
       if (httpCode == HTTP_CODE_OK) // if version received
       {
         payload = https.getString(); // save received version
-      } else {
+      }
+      else
+      {
         Serial.print("error in downloading version file:");
         Serial.println(httpCode);
       }
@@ -163,20 +171,21 @@ int FirmwareVersionCheck(void) {
     }
     delete client;
   }
-      
+
   if (httpCode == HTTP_CODE_OK) // if version received
   {
     payload.trim();
-    if (payload.equals(FirmwareVer)) {
+    if (payload.equals(FirmwareVer))
+    {
       Serial.printf("\nDevice already on latest firmware version:%s\n", FirmwareVer);
       return 0;
-    } 
-    else 
+    }
+    else
     {
       Serial.println(payload);
       Serial.println("New firmware detected");
       return 1;
     }
-  } 
-  return 0;  
+  }
+  return 0;
 }
